ea599118d1518cc2ee8e41bc4a5e57865ba668e2
Chutiendat26092003 <91305197+Chutiendat26092003@users.noreply.github.com>
2022-05-12 08:34:51 +0700

    Dat

--- MySQL/src/buoi5.java
+++ MySQL/src/buoi5.java
@@ -14,7 +14,8 @@ public class buoi5 {
                 //Các hoạt động JDBC được thực hiện thông qua các đối tượng "Connection", "Statement", và "ResultSet"
 
                 //Đầu tiên cần tạo ra đối tượng Connection, sử dụng URL cơ sở dữ liệu để kết nối với máy chủ
-                Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/ebookshop", "root", "");
+                Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/ebookshop",
+                                                         "root", "");
 
                 //Phân bổ Statement đối tượng  trong Connection thông qua conn.createStatement()
                 Statement stmt = conn.createStatement();
@@ -22,12 +23,15 @@ public class buoi5 {
             // Issue a SELECT to check the changes
             String strSelect = "select * from books";
             System.out.println("The SQL statement is: " + strSelect + "\n");
-            //Để thực thi lệnh SQL SELECT, ta gọi phương thức stmt.executeQuery(strSelect) trả về kết quả truy vấn trong ResultSet đối tượng(được gọi resultSet)
-            ResultSet resultSet = stmt.executeQuery(strSelect); //Mô hình bảng được trả về, có thể truy cập thông qua 1 con trỏ hàng
+            /*Để thực thi lệnh SQL SELECT, ta gọi phương thức stmt.executeQuery(strSelect)
+            trả về kết quả truy vấn trong ResultSet đối tượng(được gọi resultSet) */
+            ResultSet resultSet = stmt.executeQuery(strSelect); /*Mô hình bảng được trả về,
+            có thể truy cập thông qua 1 con trỏ hàng*/
             //Con trỏ ban đầu được định vị trước hàng đầu tiên trong ResultSet
             while (resultSet.next()) { // di chuyển con trỏ đến hàng đầu tiên
-                //Các cột trong mỗi hàng phải được đọc từ trái sang phải và mỗi cột chỉ được đọc 1 lần thông qua các phương thức getXXX()
-                System.out.println(resultSet.getInt("id") + ", " // 1 ô nếu getXXX() có thể gây ra lỗi lạ
+                /*Các cột trong mỗi hàng phải được đọc từ trái sang phải
+                và mỗi cột chỉ được đọc 1 lần thông qua các phương thức getXXX() */
+                System.out.println(resultSet.getInt("id") + ", "
                         + resultSet.getString("author") + ", "
                         + resultSet.getString("title") + ", "
                         + resultSet.getDouble("price") + ", "
@@ -38,7 +42,8 @@ public class buoi5 {
 
 
             // SEARCH
-            System.out.println("\n\nEnter the book  id you want to SEARCH: "); Integer enterID = sc.nextInt();
+            System.out.println("\n\nEnter the book  id you want to SEARCH: ");
+            Integer enterID = sc.nextInt();
             String strSelect2  = "select * from books  where id = " + enterID;
             System.out.println("The SQL Statement is: " + strSelect2 + "\n");
             ResultSet resultSet2 = stmt.executeQuery(strSelect2);
@@ -55,7 +60,8 @@ public class buoi5 {
 
 
             // DELETE 1 book with user-entered id
-            System.out.println("\n\nEnter the book  id you want to DELETE: "); Integer deleteID = sc.nextInt();
+            System.out.println("\n\nEnter the book  id you want to DELETE: ");
+            Integer deleteID = sc.nextInt();
             String sqlDelete = "delete from books where id = " + deleteID;
             System.out.println("The SQL statement is: " + sqlDelete + "\n");
             int countDeleted = stmt.executeUpdate(sqlDelete);
@@ -70,7 +76,11 @@ public class buoi5 {
             System.out.println("Enter author: "); String insertAuthor = sc.nextLine();
             System.out.println("Enter price: "); double insertPrice = sc.nextDouble();
             System.out.println("Enter qty: "); int insertQty = sc.nextInt();
-            String sqlInsert = "insert into books values (" + insertID + ", '" + insertTitle + "', '" + insertAuthor + "', " + insertPrice + ", " + insertQty + ")";
+            String sqlInsert = "insert into books values (" + insertID + ", '"
+                    + insertTitle + "', '"
+                    + insertAuthor + "', "
+                    + insertPrice + ", " 
+                    + insertQty + ")";
             System.out.println("The SQL statement is: " + sqlInsert + "\n");
             int countInserted = stmt.executeUpdate(sqlInsert);
             System.out.println(countInserted + " records inserted.\n");
